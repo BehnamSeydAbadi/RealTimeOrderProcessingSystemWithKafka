e7a7e56bbcc656b6136a8c444b6d69dabc3c2939
Behnam SeydAbadi <behnamsa97@protonmail.com>
2025-05-07 22:01:20 +0330

    fix: Fix the docker configurations of two services

--- src/InventoryService/InventoryService.WebApi/Program.cs
+++ src/InventoryService/InventoryService.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using InventoryService.Infrastructure;
 using InventoryService.WebApi.Inventory;
 using Mapster;
 
@@ -7,11 +8,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMapster();
 builder.Services.AddMediator(options => options.ServiceLifetime = ServiceLifetime.Scoped);
+InventoryServiceInfrastructureBootstrapper.Run(builder.Services);
 
 InventoryMapper.Register();
 ProductMapper.Register();
 
 var app = builder.Build();
+
+EnsureDatabaseCreated(app);
 
 InventoryEndpoints.Map(app);
 
@@ -19,3 +23,10 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+
+void EnsureDatabaseCreated(WebApplication webApplication)
+{
+    var serviceScope = webApplication.Services.CreateScope();
+    var orderServiceDbContext = serviceScope.ServiceProvider.GetRequiredService<InventoryServiceDbContext>();
+    orderServiceDbContext.Database.EnsureCreated();
+}
